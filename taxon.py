0d32a87f0645b21de55aa6cf88263c2a55d47c14
rdelcarlo <rdelcarlo@med.unr.edu>
2019-10-23 03:29:41 -0700

    Added function to tally diad vs non-diad

--- taxon.py
+++ taxon.py
@@ -6,8 +6,8 @@ This will report the taxon and diadromous category.
 This will then sum the log body sizes and print out the total log body sizes for each.
 """
 
-# Import the regular expression profiling
-import re
+# Create a stylistic line break unit
+dash = '-' *47
 
 # Create a variable of the filename
 InFileName = "Bloom_etal_2018_Reduced_Dataset.csv"
@@ -21,8 +21,11 @@ Line = 0
 # Create a starting point variable for log body size
 logbodysize = 0
 
-# Create a stylistic line break unit
-dash = '-' *47
+# Create an empty variable to tally diadromous entries
+diad = 0
+
+# Create an empty variable to tally non-diadromous entries
+nondiad = 0
 
 # For each line, print out the taxon and diadromous status
 for row in InFile:
@@ -32,8 +35,13 @@ for row in InFile:
 		print('{:<32s}{:<15s}'.format(row[0],row[3]))
 		size = float(row[1])
 		logbodysize = logbodysize + size
+		if row[3] == "diadromous":
+			diad += 1
+		elif row[3] == "non-diadromous":
+			nondiad += 1
 	else:
 		print('\n'+"Taxon"+'\t\t\t\t'+"Diadromous_status"+'\n'+dash)
 	Line += 1
 
 print('\n'+dash+'\n'+"Sum Log Body Size"+'\n'+dash+'\n'+str(logbodysize)+'\n')
+print(dash+'\n'+"Diadromous  vs  Non-Diadromous"+'\n'+dash+'\n'+str(diad)+'\t\t'+str(nondiad)+'\n')
